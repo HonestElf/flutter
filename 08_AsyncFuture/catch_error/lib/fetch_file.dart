8e6e5ecf75c5aa81c1d992be554b1ba9f00b39b7
Vasiliy Prokudin <vd.prokudin@jet.su>
2022-11-21 16:16:45 +0300

    finished point 1

--- 08_AsyncFuture/catch_error/lib/fetch_file.dart
+++ 08_AsyncFuture/catch_error/lib/fetch_file.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/services.dart';
 
-Future<String> fetchFileFromAssets(String assetsPath) async {
-  return rootBundle.loadString(assetsPath).then((file) => file.toString());
+Future<String>? fetchFileFromAssets(String assetsPath) async {
+  try {
+    final data =
+        await rootBundle.loadString(assetsPath).then((file) => file.toString());
+
+    return data;
+  } catch (error) {
+    return Future.error(error.toString());
+  }
 }
