23357c09b3bc15fb9b5c98829a3a03fe643840cb
Vasiliy <prokudinvasya@gmail.com>
2022-11-28 15:55:54 +0300

    add test

--- 10_Testing/testing/integration_test/app_test.dart
+++ 10_Testing/testing/integration_test/app_test.dart
@@ -44,6 +44,64 @@ void main() {
           expect(find.text('Вход'), findsWidgets);
         },
       );
+
+      testWidgets(
+        'Correct login enter',
+        (widgetTester) async {
+          await widgetTester.pumpWidget(const MyApp());
+
+          final fieldFinderEmail = find.byKey(const Key('fieldEmail'));
+          final fieldFinderPhone = find.byKey(const Key('fieldPhone'));
+          final fieldFinderSubmit = find.text('Отправить');
+
+          await widgetTester.enterText(fieldFinderEmail, 'correct@email.ru');
+          await widgetTester.enterText(fieldFinderPhone, '23423424242');
+
+          expect(find.text('correct@email.ru'), findsOneWidget);
+          expect(find.text('23423424242'), findsOneWidget);
+
+          await widgetTester.tap(fieldFinderSubmit);
+
+          await widgetTester.pump();
+
+          expect(find.text('Добро пожаловать'), findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'Correct register enter',
+        (widgetTester) async {
+          await widgetTester.pumpWidget(const MyApp());
+          final switchButton = find.byKey(const Key('SwitchButton'));
+
+          expect(switchButton, findsOneWidget);
+
+          await widgetTester.tap(switchButton);
+
+          await widgetTester.pump();
+          final fieldFinderName = find.byKey(const Key('fieldName'));
+          final fieldFinderLastname = find.byKey(const Key('fieldLastName'));
+          final fieldFinderEmail = find.byKey(const Key('fieldEmail'));
+          final fieldFinderPhone = find.byKey(const Key('fieldPhone'));
+          final fieldFinderSubmit = find.text('Отправить');
+
+          await widgetTester.enterText(fieldFinderName, 'Petr');
+          await widgetTester.enterText(fieldFinderLastname, 'Ivanov');
+          await widgetTester.enterText(fieldFinderEmail, 'correct@email.ru');
+          await widgetTester.enterText(fieldFinderPhone, '23423424242');
+
+          expect(find.text('Petr'), findsOneWidget);
+          expect(find.text('Ivanov'), findsOneWidget);
+          expect(find.text('correct@email.ru'), findsOneWidget);
+          expect(find.text('23423424242'), findsOneWidget);
+
+          await widgetTester.tap(fieldFinderSubmit);
+
+          await widgetTester.pump();
+
+          expect(find.text('Вы успешно зарегистрировались'), findsOneWidget);
+        },
+      );
     },
   );
 }
