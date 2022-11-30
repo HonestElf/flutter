1e1c7eef154e7c6a8fd383ef34bb3f56f6efb33d
Vasiliy <prokudinvasya@gmail.com>
2022-11-30 18:25:04 +0300

    added custom codegen

---
+++ flutter_application_arch/packages/module_codegen/bin/src/generator.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:dart_style/dart_style.dart';
+
+const localizationPath = 'packages/module_codegen';
+
+class Generator {
+  static const _kOutput = 'enc_variables.g.dart';
+
+  final _prologue = """
+//DO NOT EDIT. This is code generated via code generator
+
+class EnvironmentVariables {
+
+""";
+
+  final _epilogue = ' }';
+
+  String _getProperty(String value, String propertyName) => """
+String get $propertyName =>'$value';
+""";
+
+  void run({required int varNumber}) {
+    print('Generation started');
+
+    final outputFile = path.join(Directory.current.path, 'lib/$_kOutput');
+
+    final output = StringBuffer();
+
+    output.writeln(_prologue);
+
+    // for (final entry in Platform.environment.entries.take(varNumber)) {
+    //   output.writeln(_getProperty(entry.value, entry.key.toLowerCase()));
+    // }
+    for (final entry in Platform.environment.entries.take(varNumber)) {
+      output.writeln(_getProperty(entry.value, entry.key.toLowerCase()));
+    }
+
+    output.writeln(_epilogue);
+    print(output.toString());
+
+    // final formattedCode = DartFormatter().format(output.toString());
+
+    File(outputFile).writeAsStringSync(output.toString().split(r'\').join('/'));
+
+    print('Generation completed');
+  }
+}
---
+++ flutter_application_arch/packages/module_codegen/lib/enc_variables.g.dart
@@ -0,0 +1,24 @@
+//DO NOT EDIT. This is code generated via code generator
+
+class EnvironmentVariables {
+  String get flutter_root => 'C:/flutter';
+
+  String get pub_environment => 'flutter_cli:interactive';
+
+  String get pub_cache => 'C:/flutter/.pub-cache';
+
+  String get allusersprofile => 'C:/ProgramData';
+
+  String get amdrmsdkpath => 'C:/Program Files/AMD/RyzenMasterSDK/';
+
+  String get appdata => 'C:/Users/proku/AppData/Roaming';
+
+  String get cache_dir => 'C:/flutter/bin/cache';
+
+  String get chocolateyinstall => 'C:/ProgramData/chocolatey';
+
+  String get chocolateylastpathupdate => '132783579808762299';
+
+  String get chrome_crashpad_pipe_name =>
+      '//./pipe/crashpad_3060_YRKTNKXNVZTVZMEV';
+}
