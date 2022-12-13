454d86d5c1ce06190be5eb6ecbe195b9a005f916
Vasiliy <prokudinvasya@gmail.com>
2022-12-13 13:39:35 +0300

    fix by review

--- advanced_widgets_homework/lib/src/weather_widget/weather_widget.dart
+++ advanced_widgets_homework/lib/src/weather_widget/weather_widget.dart
@@ -3,6 +3,10 @@ import 'package:advanced_widgets_homework/src/theme_widget.dart';
 import 'package:advanced_widgets_homework/src/weather_widget/weather_painter.dart';
 import 'package:flutter/material.dart';
 
+enum ScaleNames { normal, big }
+
+Map<ScaleNames, double> scales = {ScaleNames.normal: 1, ScaleNames.big: 2};
+
 class WeatherWidget extends StatefulWidget {
   final double weatherCondition;
   const WeatherWidget({super.key, required this.weatherCondition});
@@ -11,39 +15,17 @@ class WeatherWidget extends StatefulWidget {
   State<WeatherWidget> createState() => _WeatherWidgetState();
 }
 
-class _WeatherWidgetState extends State<WeatherWidget>
-    with TickerProviderStateMixin {
-  late final AnimationController _controller;
-  late final Animation<double> _animation;
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    );
-
-    _animation = Tween<double>(begin: 1, end: 2)
-        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut))
-      ..addListener(() {
-        setState(() {});
-      });
-  }
+class _WeatherWidgetState extends State<WeatherWidget> {
+  double scaleRate = scales[ScaleNames.normal]!;
 
   void switchScale() {
-    if (_controller.isCompleted) {
-      _controller.reverse();
-    } else {
-      _controller.forward();
-    }
+    setState(() {
+      if (scaleRate == scales[ScaleNames.normal]) {
+        scaleRate = scales[ScaleNames.big]!;
+      } else {
+        scaleRate = scales[ScaleNames.normal]!;
+      }
+    });
   }
 
   @override
@@ -55,40 +37,42 @@ class _WeatherWidgetState extends State<WeatherWidget>
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          ScaleTransition(
-            scale: _animation,
-            alignment: Alignment.topRight,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                      color: AppThemeWidget.of(context).primaryColor),
-                  borderRadius: const BorderRadius.all(Radius.circular(20))),
-              width: 80,
-              height: 80,
-              child: CustomPaint(
-                painter: CustomWeatherPainter(
-                    weatherCondition: widget.weatherCondition),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border:
+                    Border.all(color: AppThemeWidget.of(context).primaryColor),
+                borderRadius: const BorderRadius.all(Radius.circular(20))),
+            child: AnimatedSize(
+              clipBehavior: Clip.hardEdge,
+              duration: const Duration(seconds: 1),
+              reverseDuration: const Duration(seconds: 1),
+              curve: Curves.easeIn,
+              child: SizedBox(
+                width: 80 * scaleRate,
+                height: 80 * scaleRate,
+                child: CustomPaint(
+                  painter: CustomWeatherPainter(
+                      scaleRate: scaleRate,
+                      weatherCondition: widget.weatherCondition),
+                ),
               ),
             ),
           ),
-          _controller.isCompleted
-              ? Container(
-                  margin: const EdgeInsets.only(top: 80),
-                  child: InnerShadow(
-                    blur: 10,
-                    color: AppThemeWidget.of(context).primaryColor,
-                    offset: const Offset(0, 0),
-                    child: Container(
-                      alignment: Alignment.center,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        color: Color(0xFFE9EFEC),
-                      ),
-                      height: 50,
-                      width: 160,
-                      child: const Text('Облачно, 12 градусов'),
+          scaleRate == scales[ScaleNames.big]
+              ? InnerShadow(
+                  blur: 10,
+                  color: AppThemeWidget.of(context).primaryColor,
+                  offset: const Offset(0, 0),
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      color: Color(0xFFE9EFEC),
                     ),
+                    height: 50,
+                    width: 160,
+                    child: const Text('Облачно, 12 градусов'),
                   ),
                 )
               : const SizedBox(),
