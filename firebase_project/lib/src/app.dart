e40342093d46d799cccfe2153776773ee8dbf8d1
Vasiliy Prokudin <vd.prokudin@jet.su>
2023-02-13 14:00:55 +0300

    added auth

--- firebase_project/lib/src/app.dart
+++ firebase_project/lib/src/app.dart
@@ -1,9 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_project/src/user.dart';
+import 'package:firebase_project/src/user.dart' as custom;
 
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,13 +14,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.userChanges(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const ElevatedButton(
+                  onPressed: signIn, child: const Text('Login'));
+            } else {
+              return const MyHomePage(title: 'Flutter Demo Home Page');
+            }
+          },
+        ));
   }
+}
+
+void signIn() {
+  if (kIsWeb) {
+    signInWithGoogleWeb();
+  } else {
+    signInWithGoogle();
+  }
+}
+
+Future<UserCredential?> signInWithGoogle() async {
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+  final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken, idToken: googleAuth?.idToken);
+
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
+
+Future<UserCredential?> signInWithGoogleWeb() async {
+  final GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+  googleProvider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
+
+  return await FirebaseAuth.instance.signInWithPopup(googleProvider);
 }
 
 class MyHomePage extends StatefulWidget {
@@ -35,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _amountController = TextEditingController();
 
-  late CollectionReference<User> _users;
+  late CollectionReference<custom.User> _users;
   final storage = FirebaseStorage.instance;
 
   @override
@@ -47,8 +88,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
     _users = FirebaseFirestore.instance
         .collection('birthday')
-        .withConverter<User>(
-          fromFirestore: (snapshot, options) => User.fromJson(snapshot.data()!),
+        .withConverter<custom.User>(
+          fromFirestore: (snapshot, options) =>
+              custom.User.fromJson(snapshot.data()!),
           toFirestore: (value, options) => value.toJson(),
         );
   }
@@ -62,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
       //           _amountController.text != '' ? _amountController.text : '0')));
       // });
 
-      _users.add(User(
+      _users.add(custom.User(
           name: _nameController.text,
           amount: int.parse(_amountController.text)));
     }
@@ -97,7 +139,7 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             children: <Widget>[
               Expanded(
-                  child: StreamBuilder<List<User>>(
+                  child: StreamBuilder<List<custom.User>>(
                       stream: _users.snapshots().map(
                           (event) => event.docs.map((e) => e.data()).toList()),
                       builder: (context, snapshot) {
