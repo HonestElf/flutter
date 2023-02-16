5c97056f042a9abc6f3d7c245ef058c168b669f7
Vasiliy <prokudinvasya@gmail.com>
2023-02-16 15:25:43 +0300

    started media homework

--- webview_camera_maps_playback_flutter/lib/main.dart
+++ webview_camera_maps_playback_flutter/lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_camera_maps_playback_flutter/src/camera_app/app.dart';
+import 'package:webview_camera_maps_playback_flutter/src/media_app/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const CameraApp());
+  // runApp(const CameraApp());
+  runApp(const MyMediaApp());
 }
