7356f5b7ec0c076ab8dd252ad8eab35ad4656bc9
Vasiliy <prokudinvasya@gmail.com>
2023-03-01 14:26:39 +0300

    Revert "added FFI (not working)"
    This reverts commit 92a5227643ea949cff177d85f9afb2c2c00525a5.

--- flutter_integration/lib/main.dart
+++ flutter_integration/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_integration/src/FFI/ffi_app.dart';
 import 'package:flutter_integration/src/event_channel/event_channel_app.dart';
 import 'package:flutter_integration/src/js_interop/js_interop_app.dart';
 import 'package:flutter_integration/src/platform_view/platform_view_app.dart';
@@ -7,6 +6,5 @@ import 'package:flutter_integration/src/platform_view/platform_view_app.dart';
 void main() {
   // runApp(const EventChannelApp());
   // runApp(const PlatformViewApp());
-  // runApp(const JSinteropApp());
-  runApp(const FFIApp());
+  runApp(const JSinteropApp());
 }
--- flutter_integration/lib/src/FFI/source/random.c
+++
@@ -1,9 +0,0 @@
-#include <time.h>
-#include <stdlib.h>
-
-int get_value()
-{
-   srand(time(NULL));   // Initialization, should only be called once.
-   int r = rand() % 500;
-   return r;
-}
