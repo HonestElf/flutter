287b6f9a18fae7a7064f6bf4c91bdaf40139c914
Vasiliy <prokudinvasya@gmail.com>
2023-03-07 13:58:30 +0300

    Finished second task

--- flutter_persistance_homework/lib/src/storage_app/get_image.dart
+++ flutter_persistance_homework/lib/src/storage_app/get_image.dart
@@ -3,16 +3,6 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 
-// Future<File> _downloadFile(String url, String filename) async {
-//   var request = await httpClient.getUrl(Uri.parse(url));
-//   var response = await request.close();
-//   var bytes = await consolidateHttpClientResponseBytes(response);
-//   String dir = (await getApplicationDocumentsDirectory()).path;
-//   File file = new File('$dir/$filename');
-//   await file.writeAsBytes(bytes);
-//   return file;
-// }
-
 Future<dynamic> downloadImage(String url) async {
   final response = await http.get(Uri.parse(url));
 }
@@ -21,9 +11,21 @@ Future<File> downloadFile(String url, String filename) async {
   http.Client client = http.Client();
   var req = await client.get(Uri.parse(url));
   var bytes = req.bodyBytes;
-  String dir = (await getApplicationDocumentsDirectory()).path;
+  Directory appDocDir = await getApplicationDocumentsDirectory();
+
   String prunedName = '${filename.replaceAll(' ', '')}.jpg';
-  File file = File('$dir/downloaded/$prunedName');
+  Directory targetDir = Directory('${appDocDir.path}/downloads');
+
+  await checkIfDirExist(targetDir);
+  File file = File('${targetDir.path}/$prunedName');
   await file.writeAsBytes(bytes);
+
+  print('SAVED');
   return file;
+}
+
+Future<void> checkIfDirExist(Directory targetDir) async {
+  if (!(await targetDir.exists())) {
+    await targetDir.create();
+  }
 }
--- flutter_persistance_homework/lib/src/storage_app/storage_app.dart
+++ flutter_persistance_homework/lib/src/storage_app/storage_app.dart
@@ -34,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _urlController = TextEditingController();
   final TextEditingController _textEditingController = TextEditingController();
 
-  List<File> _imageGallery = [];
+  final ValueNotifier<List<File>> _imageGallery = ValueNotifier<List<File>>([]);
   File? file;
   late Directory appDocDir;
 
@@ -47,28 +47,27 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _initStorage() async {
     appDocDir = await getApplicationDocumentsDirectory();
+    final Directory temp = Directory('${appDocDir.path}/downloads');
 
-    appDocDir.list().forEach((file) {
-      print('FILES: ${file.path}');
-    });
+    if (await temp.exists()) {
+      temp.list().forEach((file) async {
+        if (await file.exists()) {
+          _imageGallery.value = [..._imageGallery.value, File(file.path)];
+        }
+      });
+    }
   }
 
   void _saveImage() async {
-    print('URL: ${_urlController.text}');
-    print('NAME: ${_textEditingController.text}');
+    //https://picsum.photos/200
 
     file = await downloadFile(_urlController.text, _textEditingController.text);
 
-    // List<File> images = appDocDir.list().map((file) {
-    //   return File(file.path);
-    // });
-// final String = appDocDir.
-
-// List<File> images = appDocDir.list().map
-
-//     setState(() {
-//       _imageGallery = images;
-//     });
+    if (await file!.exists()) {
+      _imageGallery.value = [..._imageGallery.value, file!];
+      _urlController.clear();
+      _textEditingController.clear();
+    }
   }
 
   @override
@@ -90,20 +89,34 @@ class _MyHomePageState extends State<MyHomePage> {
               controller: _textEditingController,
               decoration: const InputDecoration(hintText: 'MyAwesomeFile'),
             ),
-            if (file != null)
-              Container(height: 100, child: Image.file(File(file!.path)))
-            // Expanded(
-            //     child: _imageGallery.isEmpty
-            //         ? const Center(child: Text('No images'))
-            //         : ListView.builder(
-            //             itemCount: _imageGallery.length,
-            //             itemBuilder: (context, index) {
-            //               return Image.file(
-            //                 File(_imageGallery[index].path),
-            //                 fit: BoxFit.cover,
-            //               );
-            //             },
-            //           ))
+            ValueListenableBuilder(
+              valueListenable: _imageGallery,
+              builder: (context, images, child) {
+                if (images.isEmpty) {
+                  return const Center(child: Text('No images'));
+                } else {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: images.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          width: 200,
+                          height: 150,
+                          padding: const EdgeInsets.all(16),
+                          decoration: const BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))),
+                          child: Image.file(
+                            File(images[index].path),
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                }
+              },
+            )
           ],
         ),
       ),
