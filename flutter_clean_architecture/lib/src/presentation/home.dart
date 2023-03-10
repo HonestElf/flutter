f8ce5b93d271b330c332e138d523ff5ccc345667
Vasiliy Prokudin <vd.prokudin@jet.su>
2023-03-10 15:12:44 +0300

    added layout

--- flutter_clean_architecture/lib/src/presentation/home.dart
+++ flutter_clean_architecture/lib/src/presentation/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/src/domain/model/day.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,39 +11,69 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final TextEditingController _latController = TextEditingController();
+  final TextEditingController _longController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  Day? _day;
+
+  void _getDay() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                    controller: _latController,
+                    keyboardType: const TextInputType.numberWithOptions(
+                        decimal: true, signed: true),
+                    decoration: const InputDecoration(hintText: 'Широта'),
+                  )),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                      child: TextField(
+                    controller: _longController,
+                    keyboardType: const TextInputType.numberWithOptions(
+                        decimal: true, signed: true),
+                    decoration: const InputDecoration(hintText: 'Долгота'),
+                  ))
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    _getDay();
+                  },
+                  child: const Text('Получить')),
+              const SizedBox(
+                height: 20,
+              ),
+              if (_day != null)
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text('Восход: ${_day!.sunrise.toLocal()}'),
+                    Text('Закат: ${_day!.sunset.toLocal()}'),
+                    Text('Полдень: ${_day!.solarNoon.toLocal()}'),
+                    Text(
+                        'Продолжительность: ${Duration(seconds: _day!.dayLength)}'),
+                  ],
+                ),
+            ],
+          ),
+        ));
   }
 }
